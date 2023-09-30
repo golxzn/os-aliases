f551da1ab6819f56c65dce64a1e2351753cd9e71
Ruslan Golovinskii <42336163+golxzn@users.noreply.github.com>
2023-09-29 22:57:39 -0700

    Initial commit
    It's initial, but maybe even final lol

---
+++ README.md
@@ -0,0 +1,5 @@
+<h1 align="center">golxzn::types</h1>
+
+## __*Description*__
+
+Just type aliases for C++ standard types, which uses in almost all of my libraries.
---
+++ code/include/golxzn/aliases.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#if !defined(GOLXZN_ALIASES)
+#define GOLXZN_ALIASES
+#endif // !defined(GOLXZN_ALIASES)
+
+#include <cstddef>
+#include <cstdint>
+
+namespace golxzn {
+
+using i8  = std::int8_t;
+using i16 = std::int16_t;
+using i32 = std::int32_t;
+using i64 = std::int64_t;
+
+using u8  = std::uint8_t;
+using u16 = std::uint16_t;
+using u32 = std::uint32_t;
+using u64 = std::uint64_t;
+
+using f32 = float;
+using f64 = double;
+
+using byte = std::byte;
+
+template<class T>
+struct static_class {
+	static_class() = delete;
+	static_class(const static_class&) = delete;
+	static_class& operator=(const static_class&) = delete;
+	static_class(static_class&&) = delete;
+	static_class& operator=(static_class&&) = delete;
+};
+
+} // namespace golxzn
+
+namespace gxzn = golxzn;
